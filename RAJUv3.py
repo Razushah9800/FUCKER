c41f644cb4d11fc4abefb73a58b778de0630a8d3
raajudon <112608607+Razushah9800@users.noreply.github.com>
2023-04-30 00:51:40 +0545

    Update RAJUv3.py

--- RAJUv3.py
+++ RAJUv3.py
@@ -2,7 +2,7 @@
 
 def modules():
 	print("\033[1;37m [\u001b[36m•\033[1;37m] CHECKING FOR UPDATES \033[1;37m")
-	os.system('pkg update -y && pkg upgrade -y')
+	os.system('pkg update -y && pkg upgrade -y') 
 	os.system('clear')
 	try:
 		import rich
